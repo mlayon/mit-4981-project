f0618188314ebe2ae3b000a26a97896a97cbce9a
Mikhaela Layon <mapl.layon@gmail.com>
2020-11-11 00:02:19 -0800

    moved config-related functions to config.c/h

--- server-config.c
+++ server-config.c
@@ -1,9 +1,12 @@
+// How to run:
+// gcc -std=c11 -Wall -Werror -pedantic -o server-config.c config.c
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include "config.h"
 
 #define BACKLOG 5
 #define BUF_SIZE 100
@@ -11,14 +14,6 @@
 // https://www.sciencedirect.com/topics/computer-science/registered-port#:~:text=Ports%200%20through%201023%20are,be%20used%20dynamically%20by%20applications.
 // /etc/services
 #define PORT 49157
-
-typedef struct {
-    int port;
-} Config;
-
-int get_config_file(Config *conf, char *filename);
-int get_config_defaults(Config *conf);
-
 
 int main(int argc, const char * argv[])
 {
@@ -51,6 +46,7 @@ int main(int argc, const char * argv[])
         return 0;
     } 
     printf("port: %d\n", conf.port);
+    printf("subprocess: %c\n", conf.subprocess);
 
 
     memset(&addr, 0, sizeof(struct sockaddr_in));
@@ -97,36 +93,4 @@ int main(int argc, const char * argv[])
     return EXIT_SUCCESS;
 }
 
-// Gets server configuration from a .config file
-// src: https://stackoverflow.com/questions/29431081/config-file-for-web-server-to-receive-get-request-in-c/29431696
-int get_config_file(Config *conf, char * filename) {
-
-    FILE *file;
-    char *line = NULL;
-    size_t len = 0;
-    ssize_t read;
-    
-    // Opening file pointer
-    file = fopen(filename, "r");
-    if (file != NULL) {
-
-        while ((read = getline(&line, &len, file)) != -1) {
-
-            // Find key names in file 
-            if (strstr(line, "port") != NULL) {
-                conf->port = atoi(line + 4);
-            }
-        }
-        fclose(file);
-        return 1;
-    }
-
-    return 0;
-}
-
-// Empty get_config function, sets server config defaults
-int get_config_defaults(Config *conf) {
-    conf->port = 49157;
-    return 1;
-}
 

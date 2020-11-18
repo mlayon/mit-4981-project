2f94c299f2cbd9ade1214821586633e61b28d59f
Tiffany Gu <tiffany-gu@hotmail.com>
2020-11-17 19:59:56 -0800

    added pthreads pool

--- README.md
+++
@@ -1,7 +0,0 @@
-Final project for COMP 4981
-
-Team Name: MIT
-
-Tiffany Gu
-Iris Law
-Mikhaela Layon
--- server.c
+++ server.c
@@ -17,163 +17,63 @@ HTTP server that serves static and
 #include <sys/wait.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <stdbool.h>
+#include <pthread.h>
+#include <stdint.h>
+// #include "threadpool.h"
 
 #define BUFSIZE 1024
 #define MAXERRS 16
-
-char *errorfile = "404.html";
-//char *html_root = "./docs/docs2";
-//default root
-char *html_root = ".";
-
-void parse_url(char filename[], char uri[], char cgiargs[]);
-void display_content(FILE *stream, int fd, char *p, char filename[], char filetype[], struct stat sbuf);
-void cerror(FILE *stream, char *cause, char *errorfile);
-size_t get_file_size(const char *filename);
-void get_error_check(FILE *stream, char m[]);
-void bind_port(int parentfd,struct sockaddr_in serveraddr, int portno);
-
-int main(int argc, char **argv)
+#define THREAD_POOL_SIZE 10
+struct node
 {
+    struct node *next;
+    int *client_socket;
+};
+typedef struct node node_t;
 
-    /* variables for connection management */
-    int parentfd;                  /* parent socket */
-    int childfd;                   /* child socket */
-    int portno;                    /* port */
-    int clientlen;                 /* byte size of client's address */
-    struct hostent *hostp;         /* client host info */
-    char *hostaddrp;               /* dotted decimal host addr string */
-    int optval;                    /* flag value for setsockopt */
-    struct sockaddr_in serveraddr; /* server address */
-    struct sockaddr_in clientaddr; /* client address */
-
-    /* variables for connection I/O */
-    FILE *stream;           /* stream version of childfd */
-    char buf[BUFSIZE];      /* message buffer */
-    char method[BUFSIZE];   /* request method get, head, post*/
-    char uri[BUFSIZE];      /* request url */
-    char version[BUFSIZE];  /* request method */
-    char filename[BUFSIZE]; /* path derived from url */
-    char filetype[BUFSIZE]; /* path derived from url */
-    char cgiargs[BUFSIZE];  /* cgi argument list */
-    char *p;                /* temporary pointer */
-    int is_static;          /* static request? */
-    struct stat sbuf;       /* file status */
-    int fd;                 /* static content filedes */
-    int pid;                /* process id from fork */
-    int wait_status;        /* status from wait */
-
-    // command lines
-    if (argc != 2)
+node_t *head = NULL;
+node_t *tail = NULL;
+pthread_t thread_pool[THREAD_POOL_SIZE];
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t condition_var = PTHREAD_COND_INITIALIZER;
+void *handle_connection(void *p_client_socket);
+void *thread_function(void *arg);
+void display_content(FILE *stream, int fd, char *p, char filename[], char filetype[], struct stat sbuf);
+void parse_url(char filename[], char uri[], char cgiargs[]);
+void enqueue(int *client_socket)
+{
+    node_t *newnode = malloc(sizeof(node_t));
+    newnode->client_socket = client_socket;
+    newnode->next = NULL;
+    if (tail == NULL)
     {
-        fprintf(stderr, "usage: %s <port>\n", argv[0]);
-        exit(1);
+        head = newnode;
     }
-    portno = atoi(argv[1]);
-
-    /* open socket descriptor */
-    parentfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (parentfd < 0)
-        error("ERROR opening socket");
-
-    /* allows us to restart server immediately */
-    optval = 1;
-    setsockopt(parentfd, SOL_SOCKET, SO_REUSEADDR,
-               (const void *)&optval, sizeof(int));
-
-    /* bind port to socket */
-bind_port( parentfd, serveraddr, portno );
-
-
-    /* getting ready to accept connection requests */
-    if (listen(parentfd, 5) < 0) /* allow 5 requests to queue up */
-        error("ERROR on listen");
-
-    /* 
-   * main loop: wait for a connection request, parse HTTP,
-   * serve requested content, close connection.
-   */
-    clientlen = sizeof(clientaddr);
-    while (1)
+    else
     {
+        tail->next = newnode;
+    }
+    tail = newnode;
+}
 
-        /* wait for a connection request */
-        childfd = accept(parentfd, (struct sockaddr *)&clientaddr, &clientlen);
-        if (childfd < 0)
-            error("ERROR on accept");
-
-        /* determine who sent the message */
-        //     hostp = gethostbyaddr((const char *)&clientaddr.sin_addr.s_addr,
-        //                           sizeof(clientaddr.sin_addr.s_addr), AF_INET);
-        hostp = gethostbyname("127.0.0.1");
-        if (hostp == NULL)
-            error("ERROR on gethostbyaddr");
-        hostaddrp = inet_ntoa(clientaddr.sin_addr);
-        if (hostaddrp == NULL)
-            error("ERROR on inet_ntoa\n");
-
-        /* open the child socket descriptor as a stream */
-        if ((stream = fdopen(childfd, "r+")) == NULL)
-            error("ERROR on fdopen");
-
-        /* get the HTTP request line */
-        fgets(buf, BUFSIZE, stream);
-        //testing head
-        char bufHead[25] = "HEAD /index.html HTTP/1.1";
-     
-        printf("Request line! %s\n", buf);
-        printf("Request line head! %s\n", bufHead);
-        sscanf(buf, "%s %s %s\n", method, uri, version);
-
-        //compare GET METHOD
-        printf("COMPARE%s vs %i,", method, strcasecmp(method, "GET")); // 0 means they're requivalent
-        if (strcasecmp(method, "GET"))
+int *dequeue()
+{
+    if (head == NULL)
+    {
+        return NULL;
+    }
+    else
+    {
+        int *result = head->client_socket;
+        node_t *temp = head;
+        head = head->next;
+        if (head == NULL)
         {
-            //  getErrorCheck(stream, method);
-            close(childfd);
-            continue;
+            tail = NULL;
         }
-
-
-        /* read (and ignore) the HTTP headers */
-        fgets(buf, BUFSIZE, stream);
-        while (strcmp(buf, "\r\n"))
-        {
-            fgets(buf, BUFSIZE, stream);
-            printf("%s", buf);
-        }
-
-        /* parse the url (/filename.html)] */
-        if (!strstr(uri, "cgi-bin"))
-        { /* static content */
-            is_static = 1;
-
-            parse_url(filename, uri, cgiargs);
-        }
-
-        /* make sure the file exists */
-        if (stat(filename, &sbuf) < 0)
-        {
-            cerror(stream, filename, errorfile);
-            fclose(stream);
-            close(childfd);
-            continue;
-        }
-
-        /* Display content */
-        if (is_static)
-        {
-             //   if(strcasecmp(method, "HEAD")){        
-                int sizeHead = get_file_size(filename);
-               printf("HEAD size of file:%d " , sizeHead);
-         //   } else{   
-            display_content(stream, fd, p, filename, filetype, sbuf);
-          //  }
-        }
-
-        /* clean up */
-        fclose(stream);
-        close(childfd);
+        free(temp);
+        return result;
     }
 }
 
@@ -186,43 +86,132 @@ void error(char *msg)
     exit(1);
 }
 
-/**
- * Get the size of a file.
- * @return The filesize, or 0 if the file does not exist.
- */
-size_t get_file_size(const char *filename)
-{
-    struct stat st;
-    if (stat(filename, &st) != 0)
-    {
-        return 0;
-    }
-    return st.st_size;
-}
 /*
  * cerror - returns an error message to the client
  */
-void cerror(FILE *stream, char *cause, char *errorfile)
+void cerror(FILE *stream, char *cause, char *errno,
+            char *shortmsg, char *longmsg)
 {
-
-    int size404 = get_file_size(errorfile);
-    /* print response header */
-    fprintf(stream, "HTTP/1.1 200 OK\n");
-    fprintf(stream, "Content-length: %d\n", size404);
+    fprintf(stream, "HTTP/1.1 %s %s\n", errno, shortmsg);
     fprintf(stream, "Content-type: text/html\n");
-    fprintf(stream, "\r\n");
-    fflush(stream);
-
-    /* Use mmap to return arbitrary-sized response body */
-    int fd = open(errorfile, O_RDONLY);
-    char *p = mmap(0, size404, PROT_READ, MAP_PRIVATE, fd, 0);
-    fwrite(p, 1, size404, stream);
-    munmap(p, size404);
+    fprintf(stream, "\n");
+    fprintf(stream, "<html><title>Error</title>");
+    fprintf(stream, "<body bgcolor="
+                    "ffffff"
+                    ">\n");
+    fprintf(stream, "%s: %s\n", errno, shortmsg);
+    fprintf(stream, "<p>%s: %s\n", longmsg, cause);
 }
 
-void get_error_check(FILE *stream, char m[])
+void *thread_function(void *arg)
 {
-    cerror(stream, m, errorfile);
+    while (true)
+    {
+
+        int *pclient;
+        pthread_mutex_lock(&mutex);
+        pthread_cond_wait(&condition_var, &mutex);
+        pclient = dequeue();
+        pthread_mutex_unlock(&mutex);
+
+        if (pclient != NULL)
+        {
+            //we have a connection
+            handle_connection(pclient);
+        }
+    }
+}
+
+void *handle_connection(void *p_client_socket)
+{
+
+    int childfd = *((int *)p_client_socket);
+    free(p_client_socket);
+    /* variables for connection I/O */
+    FILE *stream = NULL;    /* stream version of childfd */
+    char buf[BUFSIZE];      /* message buffer */
+    char method[BUFSIZE];   /* request method get, head, post*/
+    char uri[BUFSIZE];      /* request url */
+    char version[BUFSIZE];  /* request method */
+    char filename[BUFSIZE]; /* path derived from url */
+    char filetype[BUFSIZE]; /* path derived from url */
+    char cgiargs[BUFSIZE];  /* cgi argument list */
+    char *p = NULL;         /* temporary pointer */
+    int is_static;          /* static request? */
+    struct stat sbuf;       /* file status */
+    int fd;                 /* static content filedes */
+    int pid;                /* process id from fork */
+    int wait_status;        /* status from wait */
+
+    /* open the child socket descriptor as a stream */
+    if ((stream = fdopen(childfd, "r+")) == NULL)
+    {
+        error("ERROR on fdopen");
+    }
+
+    /* get the HTTP request line */
+    fgets(buf, BUFSIZE, stream);
+    //testing head
+    //   char bufHead[25] = "HEAD /index.html HTTP/1.1";
+    //  memcpy(buf,bufHead, strlen(bufHead));
+    printf("Request line!%s", buf);
+    sscanf(buf, "%s %s %s\n", method, uri, version);
+
+    //compare GET METHOD
+    printf("COMPARE%s vs %i,", method, strcasecmp(method, "GET")); // 0 means they're requivalent
+    if (strcasecmp(method, "GET"))
+    {
+        printf("Yoo");
+        //  getErrorCheck(stream, method);
+        close(childfd);
+        // continue;
+    }
+
+    /* read (and ignore) the HTTP headers */
+    fgets(buf, BUFSIZE, stream);
+    while (strcmp(buf, "\r\n"))
+    {
+        fgets(buf, BUFSIZE, stream);
+        printf("%s", buf);
+    }
+
+    /* parse the url (/filename.html)] */
+    if (!strstr(uri, "cgi-bin"))
+    { /* static content */
+        is_static = 1;
+
+        parse_url(filename, uri, cgiargs);
+    }
+
+    /* make sure the file exists */
+    if (stat(filename, &sbuf) < 0)
+    {
+        cerror(stream, filename, "404", "Not found",
+               "Could not find file.");
+
+        close(childfd);
+        fclose(stream);
+        return NULL;
+        // continue;
+    }
+
+    /* Display content */
+    if (is_static)
+    {
+        display_content(stream, fd, p, filename, filetype, sbuf);
+    }
+
+    /* clean up */
+    close(childfd);
+    fclose(stream);
+
+    fprintf(stderr, "closing connection\n");
+    return NULL;
+}
+void getErrorCheck(FILE *stream, char m[])
+{
+    cerror(stream, m, "501", "Not Implemented",
+           "Does not implement this method");
     fclose(stream);
 }
 
@@ -252,15 +241,53 @@ void display_content(FILE *stream, int fd, char *p, char filename[], char filety
 void parse_url(char filename[], char uri[], char cgiargs[])
 {
     strcpy(cgiargs, "");
-    strcpy(filename, html_root);
+    strcpy(filename, ".");
     strcat(filename, uri);
-    //  printf("Uri: %s", uri); // this is the url
+    printf("Uri: %s", uri); // this is the url
     if (uri[strlen(uri) - 1] == '/')
         strcat(filename, "index.html");
 }
 
-void bind_port(int parentfd,struct sockaddr_in serveraddr, int portno ){
+int main(int argc, char **argv)
+{
 
+    /* variables for connection management */
+    int parentfd;                  /* parent socket */
+    intptr_t childfd;              /* child socket */
+    int portno;                    /* port */
+    int optval;                    /* flag value for setsockopt */
+    int clientlen;                 /* byte size of client's address */
+    struct sockaddr_in serveraddr; /* server address */
+    struct sockaddr_in clientaddr; /* client address */
+    struct hostent *hostp = NULL;  /* client host info */
+    char *hostaddrp = NULL;        /* dotted decimal host addr string */
+
+    // command lines
+    if (argc != 2)
+    {
+        fprintf(stderr, "usage: %s <port>\n", argv[0]);
+        exit(1);
+    }
+    portno = atoi(argv[1]);
+    //first off create a bunch of threads to handle future connections.
+    for (int i = 0; i < THREAD_POOL_SIZE; i++)
+    {
+        pthread_create(&thread_pool[i], NULL, thread_function, NULL);
+    }
+
+    // threadpool *tPool = NULL;
+    // tPool = create_threadpool(THREAD_POOL_SIZE);
+    /* open socket descriptor */
+    parentfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (parentfd < 0)
+        error("ERROR opening socket");
+
+    /* allows us to restart server immediately */
+    optval = 1;
+    setsockopt(parentfd, SOL_SOCKET, SO_REUSEADDR,
+               (const void *)&optval, sizeof(int));
+
+    /* bind port to socket */
     bzero((char *)&serveraddr, sizeof(serveraddr));
     serveraddr.sin_family = AF_INET;
     serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
@@ -269,4 +296,45 @@ void bind_port(int parentfd,struct sockaddr_in serveraddr, int portno ){
              sizeof(serveraddr)) < 0)
         error("ERROR on binding");
 
+    /* getting ready to accept connection requests */
+    if (listen(parentfd, 1) < 0) /* allow 100 requests to queue up */
+        error("ERROR on listen");
+    clientlen = sizeof(clientaddr);
+    /* 
+   * main loop: wait for a connection request, parse HTTP,
+   * serve requested content, close connection.
+   */
+    int i = 0;
+    while (true)
+    {
+
+        /* wait for a connection request */
+        childfd = accept(parentfd, (struct sockaddr *)&clientaddr, &clientlen);
+        if (childfd < 0)
+            error("ERROR on accept");
+
+        /* determine who sent the message */
+        //     hostp = gethostbyaddr((const char *)&clientaddr.sin_addr.s_addr,
+        //                           sizeof(clientaddr.sin_addr.s_addr), AF_INET);
+        hostp = gethostbyname("127.0.0.1");
+        if (hostp == NULL)
+            error("ERROR on gethostbyaddr");
+        hostaddrp = inet_ntoa(clientaddr.sin_addr);
+        if (hostaddrp == NULL)
+            error("ERROR on inet_ntoa\n");
+
+        // dispatch(tPool, (dispatch_fn)handle_connection, (void *)childfd);
+        fprintf(stderr, "%d%s", i, " this is a test");
+        i++;
+
+        int *pclient = malloc(sizeof(int));
+        *pclient = childfd;
+
+        //make sure only one thread messes with the queue at a time
+        pthread_mutex_lock(&mutex);
+        enqueue(pclient);
+        pthread_cond_signal(&condition_var);
+        pthread_mutex_unlock(&mutex);
+    }
+    return 0;
 }
